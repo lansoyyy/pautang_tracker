3217a92fd47e5514f19fb19ed64fc4ebdf58c0c5
Lans12345 <olanalans12345@gmail.com>
2025-04-05 11:18:47 +0800

    wip dashboard ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:pautang_tracker/utils/colors.dart';
+import 'package:pautang_tracker/widgets/text_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      drawer: SizedBox(),
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: primary,
+        title: TextWidget(
+          text: 'Dashboard',
+          fontSize: 18,
+          fontFamily: 'Bold',
+          color: Colors.white,
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
