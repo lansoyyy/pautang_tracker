c6cd12b3b6e19cd6fe911169af65841d8766f0f9
Lans12345 <olanalans12345@gmail.com>
2025-04-08 09:02:40 +0800

    wip utang tab

--- lib/screens/tabs/utang_tab.dart
+++ lib/screens/tabs/utang_tab.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:pautang_tracker/utils/colors.dart';
 import 'package:pautang_tracker/widgets/drawer_widget.dart';
 import 'package:pautang_tracker/widgets/text_widget.dart';
 
-class UtangTab extends StatelessWidget {
+class UtangTab extends StatefulWidget {
   const UtangTab({super.key});
 
   @override
+  State<UtangTab> createState() => _UtangTabState();
+}
+
+class _UtangTabState extends State<UtangTab> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
+
+  final List<String> items = [
+    'All Utang',
+    'One Time Utang',
+    'Installment Utang'
+  ];
+  final String? selectedItem = 'All Utang';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+        ),
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
       drawer: DrawerWidget(),
       appBar: AppBar(
         foregroundColor: Colors.white,
@@ -21,6 +47,187 @@ class UtangTab extends StatelessWidget {
         ),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: DropdownButtonFormField<String>(
+                    value: selectedItem,
+                    items: items.map((item) {
+                      return DropdownMenuItem<String>(
+                        value: item,
+                        child: Text(
+                          item,
+                          style: TextStyle(fontFamily: 'Medium'),
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      // Handle the change
+                      print('Selected: $value');
+                    },
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 0.5),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: Colors.black, width: 0.5),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 12, vertical: 10),
+                    ),
+                  ),
+                ),
+                TextWidget(
+                  text: formattedDate,
+                  fontSize: 18,
+                  fontFamily: 'Medium',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 45,
+                  width: 310,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 0.5,
+                      ),
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Regular',
+                          fontSize: 14),
+                      onChanged: (value) {
+                        setState(() {
+                          nameSearched = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                          ),
+                          hintText: 'Search borrower',
+                          hintStyle:
+                              TextStyle(fontFamily: 'Regular', fontSize: 18),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                          )),
+                      controller: searchController,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                IconButton(
+                  onPressed: () {
+                    _selectDate(context);
+                  },
+                  icon: Icon(
+                    Icons.calendar_month,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.account_circle,
+                        size: 50,
+                      ),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextWidget(
+                            text: 'John Doe',
+                            fontSize: 18,
+                            fontFamily: 'Bold',
+                          ),
+                          TextWidget(
+                            text: 'Due Date: January 25, 2025',
+                            fontSize: 12,
+                            fontFamily: 'Regular',
+                          ),
+                        ],
+                      ),
+                      trailing: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextWidget(
+                            text: 'P5,499',
+                            fontSize: 24,
+                            fontFamily: 'Bold',
+                            color: Colors.green,
+                          ),
+                          TextWidget(
+                            text: 'Payment',
+                            fontSize: 12,
+                            fontFamily: 'Regular',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                );
+              },
+            ))
+          ],
+        ),
+      ),
     );
+  }
+
+  String formattedDate = 'No date selected';
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (picked != null) {
+      setState(() {
+        formattedDate =
+            DateFormat('MMMM dd, yyyy').format(picked); // format here
+      });
+    }
   }
 }
