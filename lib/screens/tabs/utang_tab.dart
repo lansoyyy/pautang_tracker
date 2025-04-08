6eadfacedc7aa6c1e9aaaff62865ea7d7c51d4ca
Lans12345 <olanalans12345@gmail.com>
2025-04-08 09:06:36 +0800

    added utang tab ui

--- lib/screens/tabs/utang_tab.dart
+++ lib/screens/tabs/utang_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:pautang_tracker/utils/colors.dart';
 import 'package:pautang_tracker/widgets/drawer_widget.dart';
@@ -159,51 +160,110 @@ class _UtangTabState extends State<UtangTab> {
             ),
             Expanded(child: ListView.builder(
               itemBuilder: (context, index) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: Icon(
-                        Icons.account_circle,
-                        size: 50,
+                return Slidable(
+                  endActionPane: ActionPane(
+                    motion: ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {},
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                        label: 'Edit',
                       ),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextWidget(
-                            text: 'John Doe',
-                            fontSize: 18,
-                            fontFamily: 'Bold',
-                          ),
-                          TextWidget(
-                            text: 'Due Date: January 25, 2025',
-                            fontSize: 12,
-                            fontFamily: 'Regular',
-                          ),
-                        ],
+                      SlidableAction(
+                        onPressed: (context) {
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                    title: const Text(
+                                      'Delete Confirmation',
+                                      style: TextStyle(
+                                          fontFamily: 'Bold',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    content: const Text(
+                                      'Are you sure you want to delete this Record?',
+                                      style: TextStyle(fontFamily: 'Regular'),
+                                    ),
+                                    actions: <Widget>[
+                                      MaterialButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(true),
+                                        child: const Text(
+                                          'Close',
+                                          style: TextStyle(
+                                              fontFamily: 'Regular',
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                      MaterialButton(
+                                        onPressed: () async {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text(
+                                          'Continue',
+                                          style: TextStyle(
+                                              fontFamily: 'Regular',
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ],
+                                  ));
+                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
                       ),
-                      trailing: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextWidget(
-                            text: 'P5,499',
-                            fontSize: 24,
-                            fontFamily: 'Bold',
-                            color: Colors.green,
-                          ),
-                          TextWidget(
-                            text: 'Payment',
-                            fontSize: 12,
-                            fontFamily: 'Regular',
-                          ),
-                        ],
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ListTile(
+                        leading: Icon(
+                          Icons.account_circle,
+                          size: 50,
+                        ),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextWidget(
+                              text: 'John Doe',
+                              fontSize: 18,
+                              fontFamily: 'Bold',
+                            ),
+                            TextWidget(
+                              text: 'Due Date: January 25, 2025',
+                              fontSize: 12,
+                              fontFamily: 'Regular',
+                            ),
+                          ],
+                        ),
+                        trailing: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextWidget(
+                              text: 'P5,499',
+                              fontSize: 24,
+                              fontFamily: 'Bold',
+                              color: Colors.green,
+                            ),
+                            TextWidget(
+                              text: 'Payment',
+                              fontSize: 12,
+                              fontFamily: 'Regular',
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Divider(),
-                  ],
+                      Divider(),
+                    ],
+                  ),
                 );
               },
             ))
