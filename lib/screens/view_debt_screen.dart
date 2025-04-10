b35f60fcbdeee9f7f687a81d55adfacffb5fb72e
Lans12345 <olanalans12345@gmail.com>
2025-04-10 08:27:48 +0800

    wip utang screen

---
+++ lib/screens/view_debt_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewDebtScreen extends StatelessWidget {
+  const ViewDebtScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
