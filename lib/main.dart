589c56a18230514495c5f988e7cc524632b651a3
Lans12345 <olanalans12345@gmail.com>
2025-04-16 11:37:18 +0800

    wip titlke changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Utang Tracker App',
+      title: 'tang Tracker App',
       home: const SplashScreen(),
     );
   }
